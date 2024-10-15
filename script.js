ea3292b759c82beede7249a69a729202768ea884
Ali Smith <aysmith@users.noreply.github.com>
2024-10-15 17:08:58 -0400

    feat: add getComputerChoice function to randomly return rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function getComputerChoice() {
+    // Generate a random number between 0 and 2
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    // Return "rock", "paper", or "scissors" based on the random number
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
